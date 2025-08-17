5886d4274d6068919cecb01b947a671748b3840d
Divya1209-sys <divya2190@gmail.com>
2025-08-16 21:40:40 -0700

    Add files via upload
    Day1

---
+++ day1/EmployeeDetails.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class EmployeeDetails {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       String name="John Doe";
+       int age = 30;
+       double salary = 55000.75;
+       String gender = "M";
+       boolean acceptTerms = true;
+       float rating=4.5f;
+       long mobileNumber = 9876543210L;
+       int pincode = 56001;
+       short luckynumber = 7;
+       System.out.println("EMPLOYEE DETAILS"+"\n"+"****************"+"\n"+"Name: "+name+"\n"+"Age: "+age+"\n"+"salary: "+salary+"\n"+"Gender: "+gender+"\n"+"Rating: "+rating+"\n"+"Phone Number"+mobileNumber+"\n"+"Pincode: "+pincode+"\n"+"Lucky Number: "+luckynumber+"\n");
+       
+	}
+
+}
